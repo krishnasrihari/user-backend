de47a51bbe1b4043cc0e0d3ea9cbd8f89835dd0d
Krishna Srihari <krishna.srihari@gmail.com>
2017-02-01 16:37:09 +0530

    Friendship model init

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,6 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
+  it { should have_many(:friends) }
   it {should validate_presence_of(:first_name)}
   it {should validate_presence_of(:last_name)}
 end
