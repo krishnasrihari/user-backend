82162cd386559cfe0c609b3ef6c8accdda19d1c1
Krishna Srihari <krishna.srihari@gmail.com>
2017-02-03 12:43:35 +0530

    Add heroku domain for emailer

--- config/initializers/mailer.rb
+++ config/initializers/mailer.rb
@@ -3,7 +3,7 @@ Rails.application.config.action_mailer.delivery_method = :smtp
 Rails.application.config.action_mailer.smtp_settings = {
   address:              'smtp.gmail.com',
   port:                 587,
-  domain:               'example.com',
+  domain:               'morning-forest-10892.herokuapp.com',
   user_name:            ENV['EMAIL_NAME'],
   password:             ENV['EMAIL_PASSWORD'],
   authentication:       'plain',
